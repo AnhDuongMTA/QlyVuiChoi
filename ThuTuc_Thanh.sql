9fcb585e8b847630605b274a5c2aba4c587a33aa
Thanh <35843882+VuongThanhMTA@users.noreply.github.com>
2018-05-09 13:06:29 +0700

    [THANH] inset data

--- ThuTuc_Thanh.sql
+++ ThuTuc_Thanh.sql
@@ -29,3 +29,52 @@ BEGIN
 	DELETE dbo.ChiTietPhieuMua
 	WHERE Ma_Phieu =@MaPhieu
 END 
+
+
+--- INSERT 
+INSERT dbo.PhieuMua( Ma_Phieu, Ngay_Mua, Ma_KH )
+VALUES  ( 'PM001',  GETDATE(),'KH019')
+INSERT dbo.PhieuMua( Ma_Phieu, Ngay_Mua, Ma_KH )
+VALUES  ( 'PM004',  GETDATE(),'KH018')
+INSERT dbo.PhieuMua( Ma_Phieu, Ngay_Mua, Ma_KH )
+VALUES  ( 'PM005',  GETDATE(),'KH017')
+INSERT dbo.PhieuMua( Ma_Phieu, Ngay_Mua, Ma_KH )
+VALUES  ( 'PM007',  GETDATE(),'KH016')
+INSERT dbo.PhieuMua( Ma_Phieu, Ngay_Mua, Ma_KH )
+VALUES  ( 'PM008',  GETDATE(),'KH015')
+
+
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM001' ,'DV001' , 1 ,10000 , 10000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM001' ,'DV002' , 10 ,1000 , 10000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM001' ,'DV003' ,2 ,5000 , 10000)
+
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM004' ,'DV001' , 1 ,10000 , 10000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM004' ,'DV002' , 10 ,1000 , 10000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM004' ,'DV003' , 3 ,5000 , 15000)
+
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM005' ,'DV001' , 2 ,10000 , 20000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM005' ,'DV002' , 10 ,1000 , 10000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM005' ,'DV003' , 3 ,5000 , 15000)
+
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM007' ,'DV001' , 1 ,10000 , 10000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM007' ,'DV002' , 20 ,1000 , 20000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM007' ,'DV003' , 2 ,5000 , 10000)
+
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM008' ,'DV001' , 3 ,10000 , 30000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM008' ,'DV002' , 20 ,1000 , 20000)
+INSERT dbo.ChiTietPhieuMua( Ma_Phieu ,Ma_DV , SoLuong , DonGia ,ThanhTien)
+VALUES  ( 'PM008' ,'DV003' , 2 ,5000 , 10000)
